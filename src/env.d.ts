de63fec457c9d01434fe9d90938aa3fb5f5759cf
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-02-01 17:58:41 +0100

    Add commit info to footer

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  PUBLIC_VERCEL_ENV: "development" | "preview" | "production";
+  PUBLIC_VERCEL_URL: string;
+  PUBLIC_VERCEL_GIT_COMMIT_REF: string;
+  PUBLIC_VERCEL_GIT_COMMIT_SHA: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
