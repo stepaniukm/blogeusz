0f1d5ddf6324206013b79d4363d009a3e6e2c7f0
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-10-31 14:44:39 +0100

    feat: add custom syntax highlighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
+import theme from "./horizon-italic.json";
 
 // https://astro.build/config
 export default defineConfig({
@@ -28,7 +29,7 @@ export default defineConfig({
       ],
     ],
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme,
       wrap: true,
     },
     extendDefaultPlugins: true,
