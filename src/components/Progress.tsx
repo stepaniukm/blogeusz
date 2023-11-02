2cf94290ece5f3202e9d90f90f4f94af95a4cfa3
Mateusz Stepaniuk <stepaniuk.m05@gmail.com>
2023-11-02 15:08:56 +0100

    fix: hamburger menu

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useTransition, useMemo } from "react";
 
 function useReadingProgress() {
   const [completion, setCompletion] = useState(0);
-  const [isPending, setTransition] = useTransition();
+  const [_, setTransition] = useTransition();
   useEffect(() => {
     function updateScrollCompletion() {
       const currentProgress = window.scrollY;
@@ -15,30 +15,23 @@ function useReadingProgress() {
         });
       }
     }
-    // add scroll event listener
     document.addEventListener("scroll", updateScrollCompletion);
 
-    // remove scroll event listener on umount
     return () => {
       document.removeEventListener("scroll", updateScrollCompletion);
     };
   }, []);
-  return useMemo(
-    () => [completion, isPending] as const,
-    [completion, isPending]
-  );
+  return useMemo(() => completion, [completion]);
 }
 
 export default function Progress() {
-  const [completion, isPending] = useReadingProgress();
+  const completion = useReadingProgress();
   return (
-    <div className="bg-gray-200 rounded h-2 w-full sticky top-0 left-0">
+    <div className="rounded h-2 w-full sticky top-0 left-0">
       <div
         className="bg-skin-accent rounded h-full transition-width"
         style={{ width: `${completion}%` }}
-      >
-        {isPending}
-      </div>
+      ></div>
     </div>
   );
 }
